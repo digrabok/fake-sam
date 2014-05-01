1f58fee169f2e9919d1195c858c078c47a8ca6c6
digrabok <digrabok@gmail.com>
2014-05-01 12:21:48 +0400

    version 1.0

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ fake-sam
 ========
 
 ServerAuthModule (JSR 196) implementation allow configure remoteUser (javax.servlet.http.HttpServletRequest#getRemoteUser) and list of user security groups in runtime, without app server restart.
+
+Usage (Glassfish 3)
+-------------------
+Put jar with SAM in glassfish lib directory ($AP_HOME/glassfish/lib).
+Connect to Glassfish admin panel and in security configuration page add HttpServlet based Message Security provider:
+![Edit Provider Configuration](https://cloud.githubusercontent.com/assets/2523981/2850742/fa9d0054-d107-11e3-84f4-e0757a75f40d.png)
+In properties set <strong>login</strong> and <strong>groupNames (comma separated list)</strong>
---
+++ src/main/java/com/digrabok/security/auth/message/module/DevelopmentServerAuthModule.java
@@ -0,0 +1,93 @@
+package com.digrabok.security.auth.message.module;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.auth.message.AuthException;
+import javax.security.auth.message.AuthStatus;
+import javax.security.auth.message.MessageInfo;
+import javax.security.auth.message.MessagePolicy;
+import javax.security.auth.message.callback.CallerPrincipalCallback;
+import javax.security.auth.message.callback.GroupPrincipalCallback;
+import javax.security.auth.message.module.ServerAuthModule;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * This ServerAuthModule (JSR 196) implementation allow configure remoteUser (javax.servlet.http.HttpServletRequest#getRemoteUser) and list of user security groups in provider properties.<br />
+ * <br />
+ * In module properties you must set: <br />
+ * <strong>login</strong> - property specify <em>remoteUser</em> (javax.servlet.http.HttpServletRequest#getRemoteUser) for each request.<br />
+ * <strong>groupNames</strong> - property specify comma separated list of security groups for each request.
+ */
+public class DevelopmentServerAuthModule implements ServerAuthModule {
+    private static final String LOGIN_OPTION_NAME = "login";
+    private static final String GROUP_NAMES_OPTION_NAME = "groupNames";
+    private static final String GROUP_NAMES_DELIMITER_OPTION_NAME = ",";
+
+    private static final Class[] SUPPORTED_MESSAGE_TYPES = { HttpServletRequest.class };
+
+    private String[] parseGroups(String groupsString) {
+        groupsString = groupsString.trim().replaceAll("\\s*" + GROUP_NAMES_DELIMITER_OPTION_NAME + "\\s*", GROUP_NAMES_DELIMITER_OPTION_NAME);
+        return groupsString.split(GROUP_NAMES_DELIMITER_OPTION_NAME);
+    }
+
+    @Override
+    public void initialize(MessagePolicy requestPolicy, MessagePolicy responsePolicy, CallbackHandler handler, Map options) throws AuthException {
+        this.callbackHandler = handler;
+        this.login = (String) options.get(LOGIN_OPTION_NAME);
+        this.groupNames = parseGroups((String) options.get(GROUP_NAMES_OPTION_NAME));
+    }
+
+    @Override
+    public Class[] getSupportedMessageTypes() {
+        return SUPPORTED_MESSAGE_TYPES;
+    }
+
+    @Override
+    public AuthStatus validateRequest(MessageInfo messageInfo, Subject clientSubject, Subject serviceSubject) throws AuthException {
+        if (this.login == null) {
+            try {
+                HttpServletResponse response = (HttpServletResponse)messageInfo.getResponseMessage();
+                response.sendError(401, "Login option is empty. Please add login in security provider config.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return AuthStatus.FAILURE;
+        }
+
+        try {
+            this.callbackHandler.handle(new Callback[]{
+                    new CallerPrincipalCallback(clientSubject, this.login),
+                    new GroupPrincipalCallback(clientSubject, this.groupNames)
+            });
+        } catch (IOException e) {
+            AuthException authEx = new AuthException();
+            authEx.initCause(e);
+            throw authEx;
+        } catch (UnsupportedCallbackException e) {
+            AuthException authEx = new AuthException();
+            authEx.initCause(e);
+            throw authEx;
+        }
+
+        return AuthStatus.SUCCESS;
+    }
+
+    @Override
+    public AuthStatus secureResponse(MessageInfo messageInfo, Subject subject) throws AuthException {
+        return AuthStatus.SUCCESS;
+    }
+
+    @Override
+    public void cleanSubject(MessageInfo messageInfo, Subject subject) throws AuthException {
+
+    }
+
+    private CallbackHandler callbackHandler;
+    private String login;
+    private String[] groupNames;
+}
